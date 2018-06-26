221ff42a590aa9495cefae7b5b2ece4c34f92bbf
chenyuejun <rokey.yj.chen@gmail.com>
2018-06-26 17:06:04 +0800

    change spring Cloud version

---
+++ shop-product-service/src/main/java/com/rokey/springcloud/product/ShopProductServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rokey.springcloud.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProductServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShopProductServiceApplication.class, args);
+  }
+}
